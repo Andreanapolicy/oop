660a6f62acf292899702fd4e16fadad12cfe30bf
andreanapolicy <daa.and@mail.ru>
2021-02-14 21:44:17 +0300

    Выполнил задание copyfile + написал тесты и прогнал их

---
+++ lab1/copyfile/tests/test.bat
@@ -0,0 +1,33 @@
+@echo off
+
+set PROGRAM="%~1"
+if %PROGRAM%=="" (
+	echo Please specify path to the program
+	exit /B 1
+)
+
+set OUT="c:/dev/executable/out.txt"
+
+ 
+%PROGRAM% > %OUT%
+fc error.txt %OUT% || goto err
+
+%PROGRAM% input.txt > %OUT%
+fc error.txt %OUT% || goto err
+
+%PROGRAM% input.txt output.txt additionalFile.txt > %OUT%
+fc error.txt %OUT% || goto err
+
+%PROGRAM% emptyInputFile.txt output.txt > %OUT%
+fc emptyOutputFile.txt output.txt || goto err
+
+%PROGRAM% fillInputFile.txt output.txt > %OUT%
+fc fillOutputFile.txt output.txt || goto err
+
+
+echo Test passed
+exit /B 1
+
+:err
+echo Test failed
+exit /B 1
