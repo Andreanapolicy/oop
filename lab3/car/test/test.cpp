62929d601f3497d58b3cdb72cf840da2cff24ec0
andreanapolicy <daa.and@mail.ru>
2021-04-03 21:09:28 +0300

    Car: изменил подход к написанию тестов. Добавил новые

--- lab3/car/test/test.cpp
+++ lab3/car/test/test.cpp
@@ -4,237 +4,363 @@
 #include "../CRemoteControl.h"
 #include "../common_libs.h"
 
-TEST_CASE("Test get info on car level")
+bool ExpectedModify(CCar& car, std::function<void(CCar& car)> method, const int expectedSpeed, const int expectedGear, const CCar::Direction& expectedDirection)
 {
-	CCar car;
-	REQUIRE(car.IsEngineTurn() == false);
-	REQUIRE(car.GetGear() == 0);
-	REQUIRE(car.GetSpeed() == 0);
+	method(car);
 
-	car.TurnOnEngine();
-	REQUIRE(car.IsEngineTurn() == true);
-	car.TurnOffEngine();
-	REQUIRE(car.IsEngineTurn() == false);
+	return ((car.GetSpeed() == expectedSpeed) && (car.GetGear() == expectedGear) && (car.GetDirection() == expectedDirection));
 }
 
-TEST_CASE("Test gear on wrong value")
+bool ExpectValues(CCar& car, const int expectedSpeed, const int expectedGear, const CCar::Direction& expectedDirection)
 {
-	CCar car;
-	car.TurnOnEngine();
-
-	REQUIRE(car.GetGear() == 0);
-	REQUIRE(car.SetGear(-2) == false);
-	REQUIRE(car.SetGear(6) == false);
-	REQUIRE(car.GetGear() == 0);
+	return ((car.GetSpeed() == expectedSpeed) && (car.GetGear() == expectedGear) && (car.GetDirection() == expectedDirection));
 }
 
-TEST_CASE("Test speed on negative numbers")
+TEST_CASE("Initial information about the car")
 {
-	CCar car;
-	car.TurnOnEngine();
-	car.SetGear(1);
+	GIVEN("A car")
+	{
+		CCar car;
 
-	REQUIRE(car.SetSpeed(-1) == true);
-	REQUIRE(car.GetSpeed() == 1);
+		WHEN("engine is off")
+		{
+			car.TurnOffEngine();
 
-	REQUIRE(car.SetSpeed(-50) == false);
-	REQUIRE(car.GetSpeed() == 1);
+			THEN("gear, speed and direction are initial values")
+			{
+				REQUIRE(ExpectValues(car, 0, 0, CCar::Direction::ON_THE_SPOT));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(10); }, 10, 0, CCar::Direction::ON_THE_SPOT));
+			}
+		}
+
+		WHEN("engine is on")
+		{
+			car.TurnOnEngine();
+
+			THEN("gear, speed and direction are initial values")
+			{
+				REQUIRE(ExpectValues(car, 0, 0, CCar::Direction::ON_THE_SPOT));
+			}
+		}
+	}
 }
 
-TEST_CASE("Test car on gear 0")
+TEST_CASE("Unreachable values for gear")
 {
-	CCar car;
-	car.TurnOnEngine();
-
-	SECTION("Test car on gear 0 with 0 speed")
+	GIVEN("A car")
 	{
-		REQUIRE(car.GetGear() == 0);
-		REQUIRE(car.SetSpeed(12) == false);
-		REQUIRE(car.SetGear(0) == true);
-		REQUIRE(car.GetGear() == 0);
-		REQUIRE(car.GetDirection() == CCar::Direction::ON_THE_SPOT);
+		CCar car;
+
+		WHEN("engine is on")
+		{
+			car.TurnOnEngine();
+
+			THEN("gear can't get this values: (gear < -2) && (gear > 6)")
+			{
+				REQUIRE(ExpectValues(car, 0, 0, CCar::Direction::ON_THE_SPOT));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetGear(-2); }, 0, -2, CCar::Direction::ON_THE_SPOT));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetGear(6); }, 0, 6, CCar::Direction::ON_THE_SPOT));
+			}
+		}
 	}
+}
 
-	SECTION("Test car on transition from 0 to -1 gear")
+TEST_CASE("Unreachable values for speed(negative numbers)")
+{
+	GIVEN("A car")
 	{
-		car.SetGear(-1);
-		REQUIRE(car.GetGear() == -1);
-		REQUIRE(car.GetSpeed() == 0);
-		REQUIRE(car.GetDirection() == CCar::Direction::ON_THE_SPOT);
+		CCar car;
+
+		WHEN("engine is on and gear is 1")
+		{
+			car.TurnOnEngine();
+			car.SetGear(1);
+
+			THEN("speed can't get negative values")
+			{
+				REQUIRE(ExpectValues(car, 0, 1, CCar::Direction::ON_THE_SPOT));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(-1); }, -1, 1, CCar::Direction::ON_THE_SPOT));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(-500); }, -500, 1, CCar::Direction::ON_THE_SPOT));
+			}
+		}
 	}
+}
 
-	SECTION("Test car on transition from 0 to 1 gear")
+TEST_CASE("Test different action of car's behavior on gear 0")
+{
+	GIVEN("A car")
 	{
-		car.SetGear(0);
-		car.SetGear(1);
-		car.SetSpeed(12);
-		REQUIRE(car.GetGear() == 1);
-		REQUIRE(car.GetSpeed() == 12);
-		REQUIRE(car.GetDirection() == CCar::Direction::FORWARD);
-	}
+		CCar car;
 
-	SECTION("Test car on decrease speed on 0 gear")
-	{
-		car.SetGear(1);
-		car.SetSpeed(20);
-		car.SetGear(0);
+		WHEN("engine is on and gear is 0")
+		{
+			car.TurnOnEngine();
 
-		REQUIRE(car.GetSpeed() == 20);
-		car.SetSpeed(11);
-		REQUIRE(car.GetSpeed() == 11);
-		REQUIRE(car.SetSpeed(13) == false);
+			THEN("impossible to increase speed value")
+			{
+				REQUIRE(ExpectValues(car, 0, 0, CCar::Direction::ON_THE_SPOT));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(12); }, 12, 0, CCar::Direction::ON_THE_SPOT));
+			}
+
+			THEN("speed can be change on the same value")
+			{
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetSpeed(0); }, 0, 0, CCar::Direction::ON_THE_SPOT));
+			}
+		}
+
+		WHEN("engine is on and gear is -1")
+		{
+			car.TurnOnEngine();
+			car.SetGear(-1);
+
+			THEN("value of direction must not changed")
+			{
+				REQUIRE(ExpectValues(car, 0, -1, CCar::Direction::ON_THE_SPOT));
+			}
+		}
+
+		WHEN("engine is on and gear is 1")
+		{
+			car.TurnOnEngine();
+			car.SetGear(1);
+
+			THEN("car can increase speed and change direction on FORWARD")
+			{
+				REQUIRE(ExpectValues(car, 0, 1, CCar::Direction::ON_THE_SPOT));
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetSpeed(12); }, 12, 1, CCar::Direction::FORWARD));
+
+			}
+		}
+
+		WHEN("engine is on, gear is 0 and speed 12")
+		{
+			car.TurnOnEngine();
+			car.SetGear(1);
+			car.SetSpeed(12);
+			car.SetGear(0);
+
+			THEN("car can only decrease speed, or change on the same value")
+			{
+				REQUIRE(ExpectValues(car, 12, 0, CCar::Direction::FORWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(13); }, 13, 0, CCar::Direction::FORWARD));
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetSpeed(10); }, 10, 0, CCar::Direction::FORWARD));
+
+			}
+		}
 	}
 }
 
 TEST_CASE("Test car on gear -1")
 {
-	CCar car;
-	car.TurnOnEngine();
-
-	SECTION("Test car on gear -1 with 0 speed")
+	GIVEN("A car")
 	{
-		car.SetGear(-1);
-		REQUIRE(car.GetGear() == -1);
-		REQUIRE(car.GetSpeed() == 0);
-		REQUIRE(car.GetDirection() == CCar::Direction::ON_THE_SPOT);
-	}
+		CCar car;
 
-	SECTION("Test car on gear -1 with limit speed")
-	{
-		car.SetGear(-1);
-		REQUIRE(car.SetGear(-1) == false);
-		REQUIRE(car.SetSpeed(30) == false);
-		car.SetSpeed(20);
-		REQUIRE(car.GetSpeed() == 20);
-		REQUIRE(car.GetDirection() == CCar::Direction::BACKWARD);
-	}
+		WHEN("engine is on, gear is -1, speed 0")
+		{
+			car.TurnOnEngine();
+			car.SetGear(-1);
 
-	SECTION("Test car on transition from gear -1 to gear 1")
-	{
-		car.SetGear(-1);
-		car.SetSpeed(20);
+			THEN("speed and direction are initial values")
+			{
+				REQUIRE(ExpectValues(car, 0, -1, CCar::Direction::ON_THE_SPOT));
+			}
 
-		REQUIRE(car.SetGear(1) == false);
-		REQUIRE(car.GetGear() == -1);
-		car.SetSpeed(0);
-		REQUIRE(car.GetSpeed() == 0);
-		REQUIRE(car.SetGear(1) == true);
-		REQUIRE(car.GetGear() == 1);
-	}
+			THEN("speed can't be more than 20")
+			{
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(21); }, 21, -1, CCar::Direction::BACKWARD));
+			}
 
-	SECTION("Test car on transition from gear -1 to gear 0")
-	{
-		car.SetGear(-1);
-		car.SetSpeed(15);
-		REQUIRE(car.SetGear(0) == true);
-		REQUIRE(car.SetSpeed(17) == false);
-		REQUIRE(car.SetGear(1) == false);
-		car.SetSpeed(0);
-		REQUIRE(car.SetGear(1) == true);
-		REQUIRE(car.GetGear() == 1);
-	}
-}
+			THEN("if speed more than 0, direction have to be BACKWARD")
+			{
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetSpeed(12); }, 12, -1, CCar::Direction::BACKWARD));
+			}
 
-TEST_CASE("Test car on gear 1")
-{
-	CCar car;
-	car.TurnOnEngine();
+			THEN("car can't switch gear on the same(-1)")
+			{
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetGear(-1); }, 12, -1, CCar::Direction::BACKWARD));
+			}
+		}
 
-	SECTION("Test car on gear 1 with 0 speed")
-	{
-		car.SetGear(1);
-		REQUIRE(car.GetGear() == 1);
-		REQUIRE(car.GetSpeed() == 0);
-		REQUIRE(car.GetDirection() == CCar::Direction::ON_THE_SPOT);
-	}
+		WHEN("engine is on, gear is -1, speed 20")
+		{
+			car.TurnOnEngine();
+			car.SetGear(-1);
+			car.SetSpeed(20);
 
-	SECTION("Test car on gear 1 with limit speed")
-	{
-		car.SetGear(1);
-		REQUIRE(car.SetSpeed(40) == false);
-		car.SetSpeed(30);
-		REQUIRE(car.GetSpeed() == 30);
-		REQUIRE(car.GetDirection() == CCar::Direction::FORWARD);
-		car.SetSpeed(0);
-		REQUIRE(car.GetSpeed() == 0);
-		REQUIRE(car.GetDirection() == CCar::Direction::ON_THE_SPOT);
-	}
+			THEN("car cant change gear on 1")
+			{
+				REQUIRE(ExpectValues(car, 20, -1, CCar::Direction::BACKWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetGear(1); }, 20, 1, CCar::Direction::BACKWARD));
 
-	SECTION("Test car on transition gears")
-	{
-		car.SetGear(1);
-		car.SetSpeed(30);
+			}
+		}
 
-		REQUIRE(car.SetGear(2) == true);
-		REQUIRE(car.GetGear() == 2);
+		WHEN("engine is on, gear is -1, speed 20")
+		{
+			car.TurnOnEngine();
+			car.SetGear(-1);
+			car.SetSpeed(20);
 
-		REQUIRE(car.SetGear(3) == true);
-		REQUIRE(car.GetGear() == 3);
+			THEN("car change gear on 0, but cant change on 1 after, if speed > 0")
+			{
+				REQUIRE(ExpectValues(car, 20, -1, CCar::Direction::BACKWARD));
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetGear(0); }, 20, 0, CCar::Direction::BACKWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetGear(1); }, 20, 1, CCar::Direction::FORWARD));
 
-		car.SetGear(1);
-		car.SetSpeed(15);
-		REQUIRE(car.SetGear(-1) == false);
-		REQUIRE(car.SetGear(1) == true);
-		REQUIRE(car.GetGear() == 1);
-	}
+			}
 
-	SECTION("Test car on transition from gear -1 to gear 0")
-	{
-		car.SetGear(-1);
-		car.SetSpeed(15);
-		REQUIRE(car.SetGear(0) == true);
-		REQUIRE(car.SetSpeed(17) == false);
-		REQUIRE(car.SetGear(1) == false);
-		car.SetSpeed(0);
-		REQUIRE(car.SetGear(1) == true);
-		REQUIRE(car.GetGear() == 1);
+			THEN("change on 1 after stopping")
+			{
+				REQUIRE(ExpectValues(car, 20, -1, CCar::Direction::BACKWARD));
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetSpeed(0); }, 0, -1, CCar::Direction::ON_THE_SPOT));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetGear(1); }, 0, 1, CCar::Direction::FORWARD));
+			}
+		}
 	}
 }
 
-TEST_CASE("Test car on gear upper than 1")
+TEST_CASE("Ñar on gear upper than 1")
 {
-	CCar car;
-	car.TurnOnEngine();
-
-	SECTION("Test car switch from 1 to 5 gear and limited transition")
+	GIVEN("A car")
 	{
-		car.SetGear(1);
-		REQUIRE(car.SetSpeed(30) == true);
-		REQUIRE(car.SetGear(2) == true);
-		REQUIRE(car.SetSpeed(50) == true);
-		REQUIRE(car.SetGear(3) == true);
-		REQUIRE(car.SetSpeed(60) == true);
-		REQUIRE(car.SetGear(4) == true);
-		REQUIRE(car.SetSpeed(90) == true);
-		REQUIRE(car.SetGear(5) == true);
-		REQUIRE(car.SetSpeed(150) == true);
-		REQUIRE(car.SetSpeed(151) == false);
-		REQUIRE(car.SetGear(6) == false);
-		REQUIRE(car.SetSpeed(49) == false);
+		CCar car;
 
-		REQUIRE(car.SetSpeed(50) == true);
+		WHEN("engine is on, gear is 1, speed 0")
+		{
+			car.TurnOnEngine();
+			car.SetGear(1);
 
-		REQUIRE(car.SetGear(4) == true);
-		REQUIRE(car.SetSpeed(91) == false);
-		REQUIRE(car.SetSpeed(39) == false);
+			THEN("max speed is 30, min - 0")
+			{
+				REQUIRE(ExpectValues(car, 0, 1, CCar::Direction::ON_THE_SPOT));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(31); }, 31, 1, CCar::Direction::FORWARD));
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetSpeed(30); }, 30, 1, CCar::Direction::FORWARD));
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetSpeed(0); }, 0, 1, CCar::Direction::ON_THE_SPOT));
+			}
+		}
 
-		REQUIRE(car.SetSpeed(40) == true);
+		WHEN("engine is on, gear is 2, speed 20")
+		{
+			car.TurnOnEngine();
+			car.SetGear(1);
+			car.SetSpeed(20);
+			car.SetGear(2);
 
-		REQUIRE(car.SetGear(3) == true);
-		REQUIRE(car.SetSpeed(61) == false);
-		REQUIRE(car.SetSpeed(29) == false);
+			THEN("max speed is 50, min - 20")
+			{
+				REQUIRE(ExpectValues(car, 20, 2, CCar::Direction::FORWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(19); }, 19, 2, CCar::Direction::FORWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(51); }, 51, 2, CCar::Direction::FORWARD));
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetSpeed(50); }, 50, 2, CCar::Direction::FORWARD));
+			}
+		}
 
-		REQUIRE(car.SetSpeed(30) == true);
+		WHEN("engine is on, gear is 3, speed 30")
+		{
+			car.TurnOnEngine();
+			car.SetGear(1);
+			car.SetSpeed(20);
+			car.SetGear(2);
+			car.SetSpeed(30);
+			car.SetGear(3);
 
-		REQUIRE(car.SetGear(2) == true);
-		REQUIRE(car.SetSpeed(51) == false);
-		REQUIRE(car.SetSpeed(19) == false);
+			THEN("max speed is 60, min - 30")
+			{
+				REQUIRE(ExpectValues(car, 30, 3, CCar::Direction::FORWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(29); }, 29, 3, CCar::Direction::FORWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(61); }, 61, 3, CCar::Direction::FORWARD));
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetSpeed(60); }, 60, 3, CCar::Direction::FORWARD));
+			}
+		}
 
-		REQUIRE(car.SetSpeed(20) == true);
+		WHEN("engine is on, gear is 4, speed 40")
+		{
+			car.TurnOnEngine();
+			car.SetGear(1);
+			car.SetSpeed(20);
+			car.SetGear(2);
+			car.SetSpeed(30);
+			car.SetGear(3);
+			car.SetSpeed(40);
+			car.SetGear(4);
 
-		REQUIRE(car.SetGear(1) == true);
-		REQUIRE(car.SetSpeed(31) == false);
-		REQUIRE(car.SetSpeed(0) == true);
+			THEN("max speed is 90, min - 40")
+			{
+				REQUIRE(ExpectValues(car, 40, 4, CCar::Direction::FORWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(39); }, 39, 4, CCar::Direction::FORWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(91); }, 91, 4, CCar::Direction::FORWARD));
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetSpeed(90); }, 90, 4, CCar::Direction::FORWARD));
+			}
+		}
+
+		WHEN("engine is on, gear is 5, speed 50")
+		{
+			car.TurnOnEngine();
+			car.SetGear(1);
+			car.SetSpeed(20);
+			car.SetGear(2);
+			car.SetSpeed(30);
+			car.SetGear(3);
+			car.SetSpeed(40);
+			car.SetGear(4);
+			car.SetSpeed(50);
+			car.SetGear(5);
+
+			THEN("max speed is 150, min - 50")
+			{
+				REQUIRE(ExpectValues(car, 50, 5, CCar::Direction::FORWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(49); }, 49, 5, CCar::Direction::FORWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetSpeed(151); }, 151, 5, CCar::Direction::FORWARD));
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetSpeed(150); }, 150, 5, CCar::Direction::FORWARD));
+			}
+		}
+
+		WHEN("engine is on, gear is 5, speed 50")
+		{
+			car.TurnOnEngine();
+			car.SetGear(1);
+			car.SetSpeed(20);
+			car.SetGear(2);
+			car.SetSpeed(30);
+			car.SetGear(3);
+			car.SetSpeed(40);
+			car.SetGear(4);
+			car.SetSpeed(50);
+			car.SetGear(5);
+
+			THEN("car can't change gaer on -1 and 1")
+			{
+				REQUIRE(ExpectValues(car, 50, 5, CCar::Direction::FORWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetGear(1); }, 50, 1, CCar::Direction::FORWARD));
+				REQUIRE(!ExpectedModify(car, [](CCar& car) { car.SetGear(-1); }, 50, -1, CCar::Direction::BACKWARD));
+			}
+
+			THEN("car can change gaer on 0")
+			{
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetGear(0); }, 50, 0, CCar::Direction::FORWARD));
+			}
+
+			THEN("car can change gaer on 2")
+			{
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetGear(2); }, 50, 2, CCar::Direction::FORWARD));
+			}
+
+			THEN("car can change gaer on 3")
+			{
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetGear(3); }, 50, 3, CCar::Direction::FORWARD));
+			}
+
+			THEN("car can change gaer on 4")
+			{
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetGear(4); }, 50, 4, CCar::Direction::FORWARD));
+			}
+
+			THEN("car can change gaer on 5")
+			{
+				REQUIRE(ExpectedModify(car, [](CCar& car) { car.SetGear(5); }, 50, 5, CCar::Direction::FORWARD));
+			}
+		}
 	}
 }
 
@@ -242,7 +368,7 @@ TEST_CASE("Test car's remote controller")
 {
 	CCar car;
 
-	SECTION("Test car's remote controller. Command <Info>")
+	SECTION("Command <Info>")
 	{
 		std::istringstream iss("Info");
 		std::ostringstream oss;
