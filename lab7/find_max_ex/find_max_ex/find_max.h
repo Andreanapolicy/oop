160909d9fa768c95d84533f3b5007c217a6570fa
andreanapolicy <daa.and@mail.ru>
2021-06-06 17:58:15 +0300

    FindMaxEx: Правки. Сделал функцию findMaxEx inline

--- lab7/find_max_ex/find_max_ex/find_max.h
+++ lab7/find_max_ex/find_max_ex/find_max.h
@@ -2,7 +2,7 @@
 #include <vector>
 
 template <typename T, typename Less>
-bool FindMaxEx(const std::vector<T>& arr, T& maxValue, const Less& less)
+inline bool FindMaxEx(const std::vector<T>& arr, T& maxValue, const Less& less)
 {
 	if (arr.empty())
 	{
